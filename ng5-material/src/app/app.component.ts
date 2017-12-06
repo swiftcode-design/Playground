174b133bfe6a8f6f42a5ce291bff5409ffb59bd6
George Lopez-Ayala <swiftcode.design@gmail.com>
2017-12-05 20:03:04 -0800

    angular 5 with material

---
+++ ng5-material/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { setTimeout } from 'timers';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  answer: string = '';
+  showSpinner: boolean = false;
+  answerDisplay: boolean = false;
+
+  showAnswer(){
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.answerDisplay = true;
+      this.showSpinner = false;
+    }, 1000)
+  }
+}
