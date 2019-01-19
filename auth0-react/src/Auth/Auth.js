99f984f4fa49e2435ef0a672c630e0ec58f1b4d7
George Lopez-Ayala <swiftcode.design@gmail.com>
2019-01-18 16:04:23 -0800

    auth0 login using lock widget fully funtional

---
+++ auth0-react/src/Auth/Auth.js
@@ -0,0 +1,74 @@
+import Auth0Lock from 'auth0-lock'
+import { AUTH_CONFIG } from './auth0variables'
+import history from '../history'
+const { clientId, domain, callbackUrl } = AUTH_CONFIG
+
+export default class Auth {
+  lock = new Auth0Lock(clientId, domain, {
+    autoclose: true,
+    auth: {
+      redirectUrl: callbackUrl,
+      responseType: 'token id_token',
+      params: {
+        scope: 'openid'
+      }
+    }
+  })
+
+  constructor() {
+    this.handleAuthentication();
+    // binds functions to keep this context
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+  }
+
+  login() {
+    // Call the show method to display the widget.
+    this.lock.show();
+  }
+
+  handleAuthentication() {
+    // Add a callback for Lock's `authenticated` event
+    this.lock.on('authenticated', this.setSession.bind(this));
+    // Add a callback for Lock's `authorization_error` event
+    this.lock.on('authorization_error', (err) => {
+      alert(`Error: ${err.error}. Check the console for further details.`);
+      history.replace('/home');
+    });
+  }
+
+  setSession(authResult) {
+    if (authResult && authResult.accessToken && authResult.idToken) {
+      this.lock.getUserInfo(authResult.accessToken, (err, profile) => {
+        console.log('profile', profile.sub)
+      })
+      // Set the time that the access token will expire at
+      console.log('authResult', authResult)
+      let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+      localStorage.setItem('access_token', authResult.accessToken);
+      localStorage.setItem('id_token', authResult.idToken);
+      localStorage.setItem('expires_at', expiresAt);
+      // navigate to the home route
+      // history.replace('/home');
+    }
+  }
+
+  logout() {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+
+}
---
+++ auth0-react/src/Auth/auth0variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'swiftcode.auth0.com',
+  clientId: 'NEGQZbQieXyjj2DpsPHEH6LlY4NDGcF2',
+  callbackUrl: 'http://localhost:3000/callback'
+}
